10125e97acdfa02b6dd42ee68c54404f268734eb
absd <abhimanuesasidharan@gmail.com>
2023-11-09 14:55:06 +0530

    added pretty chat window

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,5 @@
 import {PrettyChatWindow} from 'react-chat-engine-pretty'
+
 const ChatsPage = (props) => {
     return(
     <div style={{height: '100vh'}}>
@@ -6,7 +7,7 @@ const ChatsPage = (props) => {
         projectId='2c71b9fd-8323-4171-b4ab-85946b647bdf'
         username={props.user.username}
         secret={props.user.secret}
-       style={{height:'100%'}} />
+        style={{height: '100%'}}/>
     </div>
     )
 }
